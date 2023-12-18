3e471abef3824c359624ae699dccf9689c88ca92
MyMacMini <mymacmini@MyMacMinis-Mac-mini.local>
2023-12-17 21:51:03 -0500

    fix broken btc get request

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const axios = require("axios");
 
 const app = express();
 
-const sendGetRequest = async function () {
+const cryptoUSD = async function () {
     try {
-        const response = await axios.get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+        const response = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?slug=bitcoin,ethereum',
         {headers: {
-            'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c'
+            'X-CMC_PRO_API_KEY': '5795b33a-aca5-40ae-8caf-fb062d044725'
         }
     });
 
@@ -17,10 +17,73 @@ const sendGetRequest = async function () {
     }
 };
 
-app.get("/crypto", async function(req, res){
-    const response = await sendGetRequest();
+const cryptoCAD = async function () {
+    try {
+        const response = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?slug=bitcoin,ethereum&convert=CAD',
+        {headers: {
+            'X-CMC_PRO_API_KEY': '5795b33a-aca5-40ae-8caf-fb062d044725'
+        }
+    });
 
-    let price = response.data[0].quote.USD.price.toFixed(2);
+    return response.data;
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// app.get("/bitcoin/USD", async function(req, res){
+//     const response = await cryptoUSD();
+
+//     let price = response.data[1].quote.USD.price.toFixed(2);
+
+//     res.send('$' + price);
+// });
+
+app.get('/bitcoin/:coin', async function(req, res){
+    let currency = req.params.coin;
+    let param1 = currency.toUpperCase();
+
+    let response;
+    let price;
+
+    switch(param1){
+        case "USD":
+            response = await cryptoUSD();
+            break;
+        case "CAD":
+            response = await cryptoCAD();
+            break;
+        default:
+            response = await cryptoCAD();
+            break;
+    }
+
+    price = response.data[1].quote[param1].price.toFixed(2);
+
+    res.send('$' + price);
+});
+
+
+// app.get("/bitcoin/CAD", async function(req, res){
+//     const response = await cryptoCAD();
+
+//     let price = response.data[1].quote.CAD.price.toFixed(2);
+
+//     res.send('$' + price);
+// });
+
+app.get("/ethereum/USD", async function(req, res){
+    const response = await cryptoUSD();
+
+    let price = response.data[1027].quote.USD.price.toFixed(2);
+
+    res.send('$' + price);
+});
+
+app.get("/ethereum/CAD", async function(req, res){
+    const response = await cryptoCAD();
+
+    let price = response.data[1027].quote.CAD.price.toFixed(2);
 
     res.send('$' + price);
 });
@@ -28,4 +91,3 @@ app.get("/crypto", async function(req, res){
 app.listen(3000, function(){
     console.log("Server is running on port 3000!");
 });
-
